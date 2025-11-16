8659eb6ec0a99aed8ecc131cadb883aa8c115d8e
arindom <arindom793@gmail.com>
2025-11-16 18:05:22 +0600

    uploading for tem mates

--- theory_practice.php
+++ theory_practice.php
@@ -10,25 +10,47 @@
     
     <center>
 
-    <div class="box center-wrapper">
+    <div class="box">
         <label style="font-family: Arial, Helvetica, sans-serif; font-weight: 300; font-size: 35px ">Data Comm Lab Report</label>
         <hr> 
         <p>arindom793@gmail.com Switch account</p>
         <p>The name, email, and photo associated with your Google account will be recorded when you upload files and submit this form</p>
         <hr>
         <p class="red">* Indicates required question</p>
-    </div></center>
-    <center><div class="box2 center-wrapper">
+    </div>
+    <form class="box2">
+        
         <label>Email</label><label style="color: red;"> *</label>
         <br>
         <br>
-        <input type="checkbox" class="largeCheckBox">
+        <div class="moveUp">
+                <input type="checkbox" class="largeCheckBox" id="record">
+                <label class="tt">Record arindom793@gmail.com as the email to be included with my response</label>
+        </div>  
+
 
     
+</form>
+   <form class="box2">
+        <label>Student Name</label><label class="red">*</label>  <br><br> 
+        <input type="text" value="Your answer" class="inputBorder">
     
-    </div></center>
-   
+    
+    </form>
+   <form class="box2">
+        <label>Student ID</label><label class="red">*</label>  <br><br> 
+        <input type="text" value="Your answer" class="inputBorder">
+   </form>
+   <form class="box2">
+        <label>Submit File here</label><label class="red">*</label>  <br><br>
+        <label style="font-size: 10; color;rgb(170, 170, 170);">Upload 1 supported file: PDF. Max 100 MB.</label><br> <br>
+        
+        <button class="file-button">
+              Add file
+        </button>
 
+   </form>
+</center>
 
 
  </body>
